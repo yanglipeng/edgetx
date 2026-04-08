4d98722b3bcbf8f76a869c29b381566d5ed75787
yanglipeng <15210531+yanglipeng@users.noreply.github.com>
2026-04-08 21:27:32 +0800

    Fix FlightModeData: keep spare bits, add fadeSmooth bit

--- radio/src/datastructs_private.h
+++ radio/src/datastructs_private.h
@@ -240,6 +240,7 @@ PACK(struct FlightModeData {
   NOBACKUP(char name[LEN_FLIGHT_MODE_NAME]);
   // swtch of phase[0] is not used
   int16_t swtch:10 ENUM(SwitchSources) CUST(r_swtchSrc,w_swtchSrc);
+  int16_t spare:5 SKIP;
   uint8_t fadeSmooth:1;   // 0=weighted fade, 1=smooth transition
   uint8_t fadeIn;
   uint8_t fadeOut;
