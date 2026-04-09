c4c2037647979bfb7a7ff7b6e6d3ffe93df33f55
yanglipeng <15210531+yanglipeng@users.noreply.github.com>
2026-04-09 15:16:23 +0800

    Use formula for FlightModeData CHKSIZE to support all targets

--- radio/src/datastructs.h
+++ radio/src/datastructs.h
@@ -51,7 +51,7 @@ static inline void check_struct()
   CHKSIZE(GVarData, 7);
   CHKSIZE(RFAlarmData, 2);
   CHKSIZE(TrainerData, 16);
-  CHKSIZE(FlightModeData, 45);
+  CHKSIZE(FlightModeData, 5 + 2 * MAX_GVARS + 2 * MAX_TRIMS + LEN_FLIGHT_MODE_NAME);
   CHKSIZE(CustomFunctionData, 11);
 
 #if defined(PCBX7) || defined(PCBXLITE) || defined(PCBX9LITE)
